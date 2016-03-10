a7d5285511a4426d7210b1401303b3a220ada45d
skycutecat <les.miserables309@gmail.com>
2016-03-11 00:26:22 +0800

    pass string to function and compare

--- varpass/varPasByFun.java
+++ varpass/varPasByFun.java
@@ -3,12 +3,13 @@ import java.util.*;
 
 public class varPasByFun {
 	
-	static void PassByVal(int pint,Integer pInt,String []pStr,int[] pAint,StringBuffer bstr ){
+	static void PassByVal(int pint,Integer pInt,String []pStr,int[] pAint,StringBuffer bstr,String ostr ){
 		pint=11;
 		pInt=new Integer(6);
 		pStr[0]="H";
 		pAint[0]=2;
 		bstr.append("123");
+		ostr="321";
 		
 	}
 	public static void main(String[] args) {
@@ -19,12 +20,14 @@ public class varPasByFun {
 		String []pStr={"a","b","c"};
 		int []pAint={1,2};
 		StringBuffer bstr=new StringBuffer("123");
-		PassByVal(pint,pInt,pStr,pAint,bstr);
+		String ostr="123";
+		PassByVal(pint,pInt,pStr,pAint,bstr,ostr);
 		System.out.println("Org int:10             , modified :"+pint);
 		System.out.println("Org Intger:5           , modified :"+pInt);
 		System.out.println("Org int Array [0]:5    , modified :"+pAint[0]);
 		System.out.println("Org String Array [0]:a , modified :"+pStr[0]);
 		System.out.println("Org StringBuffer:123   , modified :"+bstr);
+		System.out.println("Org ostr:123           , modified :"+ostr);
 		
 	}
 

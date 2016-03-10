4b945fad66dec91758f5d75a00c073322afc9013
skycutecat <les.miserables309@gmail.com>
2016-03-10 22:20:37 +0800

    variablePassByFunction
    Observation of int, Integear, StringArray,StringBuffer,int Arry as
    paremeter function

---
+++ varpass/varPasByFun.java
@@ -0,0 +1,31 @@
+package varpass;
+import java.util.*;
+
+public class varPasByFun {
+	
+	static void PassByVal(int pint,Integer pInt,String []pStr,int[] pAint,StringBuffer bstr ){
+		pint=11;
+		pInt=new Integer(6);
+		pStr[0]="H";
+		pAint[0]=2;
+		bstr.append("123");
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int pint=10;
+		
+		Integer pInt=new  Integer(5);
+		String []pStr={"a","b","c"};
+		int []pAint={1,2};
+		StringBuffer bstr=new StringBuffer("123");
+		PassByVal(pint,pInt,pStr,pAint,bstr);
+		System.out.println("Org int:10             , modified :"+pint);
+		System.out.println("Org Intger:5           , modified :"+pInt);
+		System.out.println("Org int Array [0]:5    , modified :"+pAint[0]);
+		System.out.println("Org String Array [0]:a , modified :"+pStr[0]);
+		System.out.println("Org StringBuffer:123   , modified :"+bstr);
+		
+	}
+
+}
